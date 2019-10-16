371c181a835e2eac27106cd4a697bc129f67adce
nagbol01 <56365882+nagbol01@users.noreply.github.com>
2019-10-16 20:52:21 +0100

    lab 3

---
+++ src/main/java/Door.java
@@ -0,0 +1,41 @@
+/**
+ * Class to represent a Door with name, state, and area.
+ */
+public class Door{
+
+  // fields with access modifiers
+  private String name;
+  private String state;
+
+
+  // methods
+  public void open() {
+    this.state = "open";
+  }
+  public void close() {
+    this.state = "close";
+  }
+
+  // constructors
+  public Door(String name, String state) {
+    this.name = name;
+    this.state = state;
+  }
+
+  // accessors
+  public String getName() {
+    return name;
+  }
+  public String getState() {
+    return state;
+  }
+
+  //mutators
+  public void setName(String newName) {
+    name = newName;
+  }
+  public void setState(String newState) {
+    state = newState;
+  }
+
+}
