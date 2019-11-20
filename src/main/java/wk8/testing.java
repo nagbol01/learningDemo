c204b3c8c9df66cf56951408136ab91432f6d850
nagbol01 <56365882+nagbol01@users.noreply.github.com>
2019-11-20 20:38:43 +0000

    week8

---
+++ src/main/java/wk8/testing.java
@@ -0,0 +1,26 @@
+package wk8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class testing {
+
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("Tom");
+    list.add("Giles");
+    list.add(1, "Laura");
+    for (
+        String s : list) {
+      System.out.println(" " + s);
+    }
+    list.remove("Tom");
+    System.out.println("after:");
+    for (
+        String s : list) {
+      System.out.println(" " + s);
+    }
+  }
+
+}
